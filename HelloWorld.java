4e68ebd5241d8d098deddd4e8538e6788d97838e
sruthi2612 <85606275+sruthi2612@users.noreply.github.com>
2023-01-23 17:04:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
 	public static void main(String args[])
 		{
-			System.out.println("Hello Everyone!!Some new changes for Git Hook page");
+			System.out.println("Hello Everyone!!Some new changes for a Git Hook page");
 		}
 }
